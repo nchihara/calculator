d7fb5f71475ca31d6f79846134f0059df318afae
nchihara <nchihara@go2group.com>
2018-10-02 10:55:09 +0900

    remove some statements

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -1,10 +1,7 @@
 package com.example.calculator;
 
-import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import	org.springframework.stereotype.Service;
 
-//@SpringBootApplication
 @Service
 public class CalculatorApplication {
 
--- src/test/java/com/example/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/example/calculator/CalculatorApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.calculator;
 
 import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
 public class CalculatorApplicationTests {
 	private CalculatorApplication calculatorApplication = new CalculatorApplication();
 
