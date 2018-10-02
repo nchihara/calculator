808746b9b4e21b169619c8d7e0bc9b6ccf21693a
nchihara <nchihara@go2group.com>
2018-10-02 10:18:36 +0900

    test

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -1,6 +1,6 @@
 package com.example.calculator;
 
-//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import	org.springframework.stereotype.Service;
 
